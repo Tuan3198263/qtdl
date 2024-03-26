ff0158d93da593c4ba845d6f2584dfe15b2a69cb
Tuan3198263 <steve29012003@gmail.com>
2024-03-26 10:12:45 +0700

    Mô tả các thay đổi

--- datphong.py
+++ datphong.py
@@ -1,80 +1,77 @@
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QDateEdit, QTextEdit, QTableWidget, QTableWidgetItem, QMessageBox, QSplitter
-from PySide6.QtCore import Qt, QDate
 from conc import*   # Import kết nối đến cơ sở dữ liệu
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QLineEdit, QPushButton, QDateEdit, QTableWidget, QTableWidgetItem, QSplitter, QMessageBox, QFormLayout
+from PyQt5.QtCore import Qt, QDate
 
 class ThuePhongWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Đặt Phòng")
-        self.resize(800, 600)
-
-
+        self.resize(1080, 720)
 
         # Tạo layout chính
         main_layout = QHBoxLayout()
 
         # Tạo splitter để chia layout thành hai phần
         splitter = QSplitter(Qt.Horizontal)
-        
 
-        # Phần nhập thông tin
+       # Phần nhập thông tin
         input_widget = QWidget()
-        input_layout = QVBoxLayout(input_widget)
-        
+        input_layout = QFormLayout(input_widget)
+        #input_layout.setFieldGrowthPolicy(QFormLayout.ExpandingFieldsGrow)
+        input_layout.setVerticalSpacing(20) 
+
         home_button = QPushButton("Home", self)
+        home_button.setFixedHeight(40)
+        home_button.setFixedWidth(60)
+        home_button.setCursor(Qt.PointingHandCursor)  
+        
         home_button.clicked.connect(self.go_to_main_window)
         input_layout.addWidget(home_button)
 
         label = QLabel("Điền thông tin đặt phòng", self)
         label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("font-size: 24px; color: #333;")
         input_layout.addWidget(label)
 
-        label_ma_khach = QLabel("Mã khách hàng:", self)
-        input_layout.addWidget(label_ma_khach)
-        self.ma_khach_edit = QLineEdit(self)
-        self.ma_khach_edit.setPlaceholderText("Nhập mã khách hàng")
-        input_layout.addWidget(self.ma_khach_edit)
+        labels = ["Mã khách hàng", "Mã phòng", "Ngày vào", "Ngày ra", "Đặt cọc"]
+        self.inputs = []
 
-        label_ma_phong = QLabel("Mã phòng:", self)
-        input_layout.addWidget(label_ma_phong)
-        self.ma_phong_edit = QLineEdit(self)
-        self.ma_phong_edit.setPlaceholderText("Nhập mã phòng")
-        input_layout.addWidget(self.ma_phong_edit)
-
-        label_ngay_vao = QLabel("Ngày vào:", self)
-        input_layout.addWidget(label_ngay_vao)
-        self.ngay_vao_edit = QDateEdit(self)
-        self.ngay_vao_edit.setDisplayFormat("yyyy-MM-dd")
-        self.ngay_vao_edit.setDate(QDate.currentDate())
-        input_layout.addWidget(self.ngay_vao_edit)
-
-        label_ngay_ra = QLabel("Ngày ra:", self)
-        input_layout.addWidget(label_ngay_ra)
-        self.ngay_ra_edit = QDateEdit(self)
-        self.ngay_ra_edit.setDisplayFormat("yyyy-MM-dd")
-        self.ngay_ra_edit.setDate(QDate.currentDate())
-        input_layout.addWidget(self.ngay_ra_edit)
-
-        label_dat_coc = QLabel("Đặt cọc:", self)
-        input_layout.addWidget(label_dat_coc)
-        self.dat_coc_edit = QLineEdit(self)
-        self.dat_coc_edit.setPlaceholderText("Nhập số tiền đặt cọc")
-        input_layout.addWidget(self.dat_coc_edit)
+        for label_text in labels:
+            label = QLabel(label_text, self)
+            label.setStyleSheet("font-size: 18px; color: #666;")
+            input_layout.addWidget(label)
+            if label_text in ["Ngày vào", "Ngày ra"]:
+                date_edit = QDateEdit(self)
+                date_edit.setCalendarPopup(True)  # Hiển thị lịch khi click vào
+                date_edit.setDate(QDate.currentDate())  # Đặt ngày mặc định là ngày hiện tại
+                input_layout.addWidget(date_edit)
+                self.inputs.append(date_edit)
+                date_edit.setFixedHeight(35)
+            else:
+                line_edit = QLineEdit(self)
+                line_edit.setPlaceholderText(f"Nhập {label_text.lower()}")
+                line_edit.setFixedHeight(35)  # Đặt kích thước tối thiểu của ô nhập dữ liệu
+                input_layout.addWidget(line_edit)
+                self.inputs.append(line_edit)
 
 
-        thue_button = QPushButton("Đặt Phòng", self)
+
+        thue_button = QPushButton("Xác nhận", self)
+        thue_button.setStyleSheet("font-size: 16px; padding: 7px 10px; background-color: #4CAF50; color: white; border: none; border-radius: 5px; max-width:200px")
+        thue_button.setCursor(Qt.PointingHandCursor)  
+        thue_button.setMinimumWidth(thue_button.sizeHint().width())
         thue_button.clicked.connect(self.thue_phong)
         input_layout.addWidget(thue_button)
-
         splitter.addWidget(input_widget)
 
         # Phần hiển thị cơ sở dữ liệu dưới dạng bảng
         db_display_widget = QWidget()
         db_display_layout = QVBoxLayout(db_display_widget)
 
-        db_label = QLabel("Dữ liệu bảng Thuê Phòng", self)
+        db_label = QLabel("Dữ liệu Thuê Phòng", self)
         db_label.setAlignment(Qt.AlignCenter)
+        db_label.setStyleSheet("font-size: 18px; color: #333;")
         db_display_layout.addWidget(db_label)
 
         self.table_widget = QTableWidget(self)
@@ -94,6 +91,7 @@ class ThuePhongWindow(QMainWindow):
 
         # Hiển thị dữ liệu ban đầu
         self.display_database()
+
 
     def display_database(self):
         try:
@@ -115,14 +113,16 @@ class ThuePhongWindow(QMainWindow):
             print("Error:", e)
 
     def thue_phong(self):
-        # Lấy thông tin từ các ô nhập liệu
-        ma_khach = self.ma_khach_edit.text()
-        ma_phong = self.ma_phong_edit.text()
-        ngay_vao = self.ngay_vao_edit.date().toString(Qt.ISODate)
-        ngay_ra = self.ngay_ra_edit.date().toString(Qt.ISODate)
-        dat_coc = self.dat_coc_edit.text()
+        self.ngay_vao_edit = QDateEdit(self)
+        self.ngay_ra_edit = QDateEdit(self)
 
-        # Kiểm tra xem có trường nào trống không
+        # Lấy thông tin từ các ô nhập liệu
+        ma_khach = self.inputs[0].text()
+        ma_phong = self.inputs[1].text()
+        ngay_vao = self.ngay_vao_edit.date().toString("yyyy-MM-dd")
+        ngay_ra = self.ngay_ra_edit.date().toString("yyyy-MM-dd")
+        dat_coc = self.inputs[4].text()
+
         if not (ma_khach and ma_phong and ngay_vao and ngay_ra and dat_coc):
             QMessageBox.warning(self, "Lỗi", "Vui lòng nhập đầy đủ thông tin.")
             return
@@ -147,9 +147,12 @@ class ThuePhongWindow(QMainWindow):
             QMessageBox.warning(self, "Lỗi", f"Lỗi khi đặt phòng: {e}")
 
     def clear_fields(self):
-        self.ma_khach_edit.clear()
-        self.ma_phong_edit.clear()
-        self.dat_coc_edit.clear()
+        for widget in self.inputs:
+            if isinstance(widget, QLineEdit):
+                widget.clear()
+            elif isinstance(widget, QDateEdit):
+                widget.setDate(QDate.currentDate())
+
         
     def go_to_main_window(self):
         # Điều hướng đến giao diện chính (main.py)
